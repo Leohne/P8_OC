7eefbacff91ae43047304940db029b0cc96b5c7c
Leohne <bricemagneville.pro@gmail.com>
2023-12-25 16:24:13 +0100

    correction animation

--- src/component/Dropdown.jsx
+++ src/component/Dropdown.jsx
@@ -9,9 +9,9 @@ function Dropdown ({title, contain}) {
       }
     return (
             <>
-                <div className="dropdown_title">
+                <div onClick={animation}  className="dropdown_title">
                     <h4 className="dropdown_margin" >{title}</h4>
-                    <i onClick={animation} className={`fa-solid fa-chevron-up fa-chevron ${click ? 'fa-rotate-by' : 'fa-rotate-0'} ${responsive ? 'fa-xs' : 'fa-xl'}`}></i>
+                    <i className={`fa-solid fa-chevron-up fa-chevron ${click ? 'fa-rotate-by' : 'fa-rotate-0'} ${responsive ? 'fa-xs' : 'fa-xl'}`}></i>
                 </div>
                     <div className={` dropdown_p--position ${click ? 'dropdown_open' : 'dropdown_close'}`}>
                         <div className='dropdown_p'>{contain}</div>
