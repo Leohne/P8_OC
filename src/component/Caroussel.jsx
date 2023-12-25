8fe0ffdf57607b24d76aefb82c326459041923f0
Leohne <bricemagneville.pro@gmail.com>
2023-12-25 16:03:55 +0100

    correction footer et condition caroussel

--- src/component/Caroussel.jsx
+++ src/component/Caroussel.jsx
@@ -8,7 +8,7 @@ function Caroussel({ annonce }) {
   const { id } = useParams() 
   const imgId = annonce.find((item) => item.id === id)
   const responsive = useMediaQuery('(max-width: 425px)');
-  
+  const alone = (imgId.pictures.length === 1)
   const [count, setcount] = useState(0)
   const Previous = () => {
     setcount((previousCount) => (previousCount - 1 + imgId.pictures.length) % imgId.pictures.length)
@@ -17,14 +17,14 @@ function Caroussel({ annonce }) {
   const Next = () => {
     setcount((nextCount) => (nextCount + 1) % imgId.pictures.length)
   }
-  
+
   return (
     <>
       <div className='imgId_contain'>       
-          <button onClick={Previous} className='imgId_previous'><i className={`fa-solid fa-chevron-left ${responsive ? 'fa-2xs' : 'fa-xl'}`}></i></button>    
+          <button onClick={Previous} className={`imgId_previous ${alone ? 'hidden' : ''}`}><i className={`fa-solid fa-chevron-left ${responsive ? 'fa-2xs' : 'fa-xl'}`}></i></button>    
           <img key={imgId.id} className='imgId_img' src={imgId.pictures[count]} alt='Photo de location' />
-          <p className='imgId_count'> {count +1} / {imgId.pictures.length} </p>
-          <button onClick={Next} className='imgId_next'><i className={`fa-solid fa-chevron-right ${responsive ? 'fa-2xs' : 'fa-xl'}`}></i></button>    
+          <p className={`imgId_count ${alone ? 'hidden' : ''}`}> {count +1} / {imgId.pictures.length} </p>
+          <button onClick={Next} className={`imgId_next ${alone ? 'hidden' : ''}`}><i className={`fa-solid fa-chevron-right ${responsive ? 'fa-2xs' : 'fa-xl'}`}></i></button>    
       </div>   
     </>
   );
